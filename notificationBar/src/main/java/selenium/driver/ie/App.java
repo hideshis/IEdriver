dcce7892cfa42bef0980fd33765b94c0e39178d9
hideshis <hideshis1990@gmail.com>
2019-01-01 16:36:48 +0900

    # This is a combination of 2 commits. # This is the 1st commit message:
    generate maven project
    
    # The commit message #2 will be skipped:
    
    # squash w/ prev. commit

---
+++ notificationBar/src/main/java/selenium/driver/ie/App.java
@@ -0,0 +1,13 @@
+package selenium.driver.ie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
